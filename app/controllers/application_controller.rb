e55a866609a7ea75feb51be34bc3c6b48a9cb4f0
yosef456 <yosef.sh.ts@gmail.com>
2016-01-28 02:30:05 +0000

    first change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render text: "<h1>hello world!</h1>"
+    render text: "<h1>Goodbye world!</h1>"
   end
 end
